786c4787322243c6d1308148718329c512e8efd5
kelvys moura <kelvyscontato@gmail.com>
2018-05-26 16:23:56 -0300

    v1.1.0.b6

---
+++ version.php
@@ -0,0 +1,45 @@
+<?php
+
+
+# ALGUMAS IDEIAS PARA PROXIMAS VERSOES
+
+
+/**
+ * 
+ *
+ * * Criar arquivo de configuração padrão
+ * * setAttr do pdo dentro do arquivo de config
+ * * Criar formas de pegar query e outros dados
+ */
+
+/**
+ * 
+ *
+ * * Criar composer do diretorio
+ * * Adicionar mais duas funções em cada crud
+ */
+
+/**
+ * 
+ *
+ * * Criar camada simples de slq para pegar informações da tabela
+ * * Criar camada de erros
+ * * Adicionar mais duas função em cada crud
+ */
+
+/**
+ * 
+ *
+ * * Criar acesso ao sqlite
+ * * Criar camada de logs
+ * * Adicionar mais uma função em cada crud
+ * * Adicionar mais uma função na camada de sql
+ */
+
+/*
+$dbe->NewTable('product')->fields([
+	'id' => 'int|ai|nn|un',
+	'name' => 'varchar:40|nn',
+	'ative' => 'bool|nn|def:0'
+]);
+*/
